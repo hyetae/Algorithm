22094e7a35667edf7d7699777a43de9b4eeb2607
hyetae <86393948+hyetae@users.noreply.github.com>
2023-07-14 10:52:04 +0900

    [Bronze I] Title: 진법 변환 2, Time: 120 ms, Memory: 34928 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -0,0 +1,13 @@
+import string
+
+N, B = map(int, input().split())
+tmp = string.digits+string.ascii_uppercase
+
+def convert(N, B) :
+    q, r = divmod(N, B)
+    if q == 0 :
+        return tmp[r] 
+    else :
+        return convert(q, B) + tmp[r]
+    
+print(convert(N, B))
