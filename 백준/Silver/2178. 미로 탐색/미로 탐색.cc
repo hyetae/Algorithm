73eda7f79dc320cbcb903b728c4beaee41633f57
hyetae <86393948+hyetae@users.noreply.github.com>
2023-11-22 08:58:44 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2072 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,52 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define MAX 100
-#define fast cin.tie(0), cout.tie(0), ios::sync_with_stdio(0)
 
-int graph[MAX][MAX] = {0,};
-int visited[MAX][MAX] = {0,};
+int n, m;
+int graph[100][100];
+bool visited[100][100] = { false, };
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
-int N, M;
-
 int bfs(int x, int y){
-    queue<pair<int, int> > q;
-    q.push(make_pair(x, y));
-    visited[x][y] = 1;
+    queue<pair<int,int>> q;
+    q.push({x, y});
+    visited[x][y] = true;
 
     while(!q.empty()){
         int a = q.front().first;
         int b = q.front().second;
         q.pop();
 
-        for(int i = 0; i < 4; i++){
+        for (int i = 0; i < 4; i++){
             int nx = a + dx[i];
             int ny = b + dy[i];
 
-            if(nx < 0 || nx >= N || ny < 0 || ny >= M)
+            if(nx < 0 || ny < 0 || nx >= n || ny >= m)
                 continue;
-            if(graph[nx][ny] == 0)
+            if(!graph[nx][ny])
                 continue;
-            if(graph[nx][ny] == 1 && visited[nx][ny] == 0){
+            if(graph[nx][ny] && !visited[nx][ny]) {
+                visited[nx][ny] = true;
+                q.push({nx, ny});
                 graph[nx][ny] = graph[a][b] + 1;
-                q.push(make_pair(nx, ny));
             }
         }
     }
-    return graph[N - 1][M - 1];
+    return graph[n - 1][m - 1];
 }
 
-int main(){
-    cin >> N >> M;
+int main() {
+    cin >> n >> m;
 
-    for(int i = 0; i < N; i++){
+    for (int i = 0; i < n; i++) {
         string str;
         cin >> str;
-
-        for(int j = 0; j < M; j++)
+        for (int j = 0; j < m; j++)
             graph[i][j] = str[j] - '0';
     }
-    cout << bfs(0, 0) << endl;
+
+
+
+    cout << bfs(0, 0);
+
     return 0;
 }
