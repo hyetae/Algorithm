5909a39b0aa43f77741e8c17c708c56efaef93be
hyetae <86393948+hyetae@users.noreply.github.com>
2024-01-17 22:44:19 +0900

    [Gold IV] Title: 팀 빌딩, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/22945. 팀 빌딩/팀 빌딩.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    int x;
+    vector<int> v(N);
+    for (int i = 0; i < N; i++) {
+        cin >> v[i];
+    }
+
+    int maxi = 0;
+    int pt1 = 0, pt2 = N - 1;
+    while (pt1 < pt2) {
+        maxi = max(maxi, (pt2 - pt1 - 1) * min(v[pt1], v[pt2]));
+
+        if (v[pt1] < v[pt2])
+            pt1++;
+        else
+            pt2--;
+    }
+
+    cout << maxi;
+
+    return 0;
+}
