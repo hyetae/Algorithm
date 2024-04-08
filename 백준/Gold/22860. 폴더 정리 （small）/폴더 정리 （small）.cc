7e6d7f8e0e8a35bd3fe324c9bfac0efb235ac836
hyetae <86393948+hyetae@users.noreply.github.com>
2024-04-08 22:00:43 +0900

    [Gold III] Title: 폴더 정리 (small), Time: 624 ms, Memory: 2468 KB -BaekjoonHub

---
+++ 백준/Gold/22860. 폴더 정리 （small）/폴더 정리 （small）.cc
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define MAX 1001
+using namespace std;
+
+int cnt = 0;
+int N, M, C, Q;
+string P, F, q;
+map<string, vector<pair<string, int>>> tree;
+set<string> s;
+
+void dfs(string target, string now) {
+    for (int i = 0; i < tree[now].size(); i++) {
+        if (!tree[now][i].second) {
+            s.insert(tree[now][i].first);
+            cnt++;
+        }
+        else
+            dfs(target, tree[now][i].first);
+    }
+
+    if (target != now)
+        return;
+
+    cout << s.size() << ' ' << cnt << '\n';
+    cnt = 0;
+    s.clear();
+    return;
+}
+
+string parser(string q) {
+    vector<string> loc;
+    string name = "";
+    for (char c: q) {
+        if (c == '/') {
+            loc.push_back(name);
+            name = "";
+        } else
+            name += c;
+    }
+    loc.push_back(name);
+    return loc.back();
+}
+
+int main() {
+    fastio;
+    cin >> N >> M;
+
+    for (int i = 0; i < N + M; i++) {
+        cin >> P >> F >> C;
+        if (tree.find(P) == tree.end()) {
+            vector<pair<string, int>> v;
+            v.push_back({F, C});
+            tree[P] = v;
+        } else {
+            tree[P].push_back({F, C});
+        }
+    }
+
+    cin >> Q;
+    while (Q--) {
+        cin >> q;
+        string target = parser(q);
+        dfs(target, target);
+    }
+
+    return 0;
+}
